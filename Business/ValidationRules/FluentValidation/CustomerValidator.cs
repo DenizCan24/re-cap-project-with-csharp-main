e667d95d355dcf517746c471db56ef96e8685585
Deniz Can <denizcan3462@hotmail.com>
2021-02-24 19:48:06 +0300

    12.ders

---
+++ Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public  class CustomerValidator: AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(p => p.CustomerName).NotEmpty();
+            RuleFor(p => p.CustomerName).MinimumLength(3);
+            RuleFor(p => p.UserId).NotEmpty();
+        }
+    }
+}
